a37de0ddf7407c97f551de15e382bd9fd6d97c6a
FaisalFida <faisalfida1995@gmail.com>
2025-06-23 14:06:19 +0500

    added summary_container file in home folder/widgets

---
+++ ecg_cad_detector/lib/view/home/widgets/summary_container.dart
@@ -0,0 +1,48 @@
+import 'package:ecg_cad_detector/res/colors/app_colors.dart';
+import 'package:ecg_cad_detector/res/images/app_images.dart';
+import 'package:flutter/material.dart';
+
+Widget SummaryContainer(
+    {final String image = 'image',
+      final double height = 100,
+      final double width = 90,
+      final String title = 'title',
+      final String number = 'number'}) {
+  return Container(
+    height: height,
+    width: width,
+    decoration: BoxDecoration(
+      color: AppColors.white,
+      borderRadius: BorderRadius.circular(20),
+      boxShadow: [
+        BoxShadow(
+          color: Colors.black.withOpacity(0.2),
+          blurRadius: 4,
+          offset: Offset(2, 2),
+        ),
+      ],
+    ),
+
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Image.asset(
+          AppImages.scope,
+          height: 30,
+
+        ),
+        Text(
+          number,
+          textAlign: TextAlign.center,
+          style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold,),
+        ),
+        Text(
+          title,
+          textAlign: TextAlign.center,
+          style: TextStyle(fontSize: 15, fontWeight: FontWeight.w600,),
+        ),
+      ],
+    ),
+  );
+}
